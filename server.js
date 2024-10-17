5e667c621d4a38002391dd3ee186aff898b02bb7
Rubenslourenco <alvesrubens926@gmail.com>
2024-10-17 17:58:26 -0300

    Crianda e testando routes post

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 import express from 'express'
+import publicRoutes from './routes/public.js'
 
 const app = express()
+app.use(express.json())
+
+app.use('/', publicRoutes)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3002, () => console.log("Servidor Rodando"))
+app.listen(3000)
